1a3149b0a7260492fe2b71dca11329737a107170
Felipe Bessa <felipe-bessa@outlook.com>
2019-12-24 19:08:57 -0300

    Exercicios Propostos da Primeita Aula

---
+++ variaveis/script-exercise-01.js
@@ -0,0 +1,18 @@
+//Declarar uma variável com seu nome
+var nome = 'Felipe';
+
+//Declarar uma variável com a sua idade
+var idade = 30;
+
+//Declarar uma variável com a sua comida favorita
+var comidaFavorita = 'Arroz com Ovo e Farinha';
+
+//Atribuir valor a sua comida favorita
+comidaFavorita = 'Frango com Quiabo e Angu';
+
+//Declarar 5 variáveis diferentes sem valores
+var carro, moto, banda, cidade, pessoa;
+
+carro = 'Charger'
+
+console.log(nome, idade, comidaFavorita, carro);
