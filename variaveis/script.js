6b6d51b1d88770900263b887eebc0d94614e5a11
Felipe Bessa <felipe-bessa@outlook.com>
2019-12-24 18:55:28 -0300

    Repositorio do primeiro capitulo

---
+++ variaveis/script.js
@@ -0,0 +1,16 @@
+var nome = 'Felipe';
+var idade =  30;
+var possuiFaculdade = true;
+
+console.log(nome, idade, possuiFaculdade, nome);
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+
+console.log(totalPreco);
+
+var sobrenome = 'Bessa',
+    cidade = 'Jabó';
+
+console.log(nome, sobrenome, cidade);
