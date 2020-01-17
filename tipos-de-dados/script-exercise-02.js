383c5ecfa684928e9e61947c0691035dad8da061
Felipe Bessa <felipe-bessa@outlook.com>
2020-01-17 20:06:17 -0300

    Completando aula 2

---
+++ tipos-de-dados/script-exercise-02.js
@@ -0,0 +1,28 @@
+//Declare uma variável contendo uma string
+var string1 = 'Thundercats';
+console.log(typeof string1);
+
+//Declare uma variável contendo um número dentro de uma string
+var string2 = '69';
+console.log(typeof string2);
+
+//Declare uma variável com sua idade
+var idade = 30;
+console.log(typeof idade);
+
+//Declare duas variáveis, uma com seu nome e outra com seu sobrenome e some as mesmas
+var nome = 'Felipe',
+    sobrenome = 'Bessa';
+
+var nomeCompleto = `${nome} ${sobrenome}`;
+
+console.log(typeof nomeCompleto);
+console.log(nomeCompleto);
+
+//Coloque a seguinte frase em uma variável: It's time
+var frase = "It's time";
+console.log(frase);
+
+//Verifique o tipo da variável que contém o seu nome
+var verificaTipoNome = typeof nome;
+console.log(verificaTipoNome);
